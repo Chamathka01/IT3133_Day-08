e7fc7ef4ab2a355d989ea7c7eba5725e907618f0
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-27 14:17:27 +0000

    Defines the navigation links.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,15 @@ import Contactus from './components/Contactus';
 function App() {
   return (
     <div className="App">
-      
+     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+     </Router> 
     </div>
   );
 }
