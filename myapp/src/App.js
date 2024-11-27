5e82c4705e443dcedfcb63b564be6fbae3c61473
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-27 14:21:59 +0000

    Import BrowserRouter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link,NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Contactus from './components/Contactus';
