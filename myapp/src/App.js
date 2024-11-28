652186267ea3a1bae6a7d1ab80a714f1b395429c
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-28 08:09:05 +0000

    Defines the navigation links for login and dashboard

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -14,6 +14,8 @@ function App() {
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
         </ul>
       </nav>
 
