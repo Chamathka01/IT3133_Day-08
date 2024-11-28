a73b9fd71fb9ec4e1acb66a321526c6d8c6bef1b
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-28 07:56:33 +0000

    Created input fields and button

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -19,4 +19,24 @@ export default function Login(){
             setUser({'error':"Please check your username and password"})
         }
     }
+
+    return(
+        <div>
+            <h1>Login here</h1>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td> <input type="text" name="username" onChange={getTextInput}/></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td> <input type="password" name="pwd" onChange={getTextInput}/></td>
+                </tr>
+                <tr>
+                    <td style={{color:'red'}} colSpan={2}>{user.error}</td>
+                </tr>
+            </table>
+            <button onClick={authenticate}>Login</button>
+        </div>
+    )
 }
