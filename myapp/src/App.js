d9cf2308537540170d9f0efa8730524336462134
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-28 09:22:29 +0000

    Modified App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,9 +15,8 @@ function App() {
         <ul>
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
           <li><Link to="/login">Login</Link></li>
-          <li><Link to="/dashboard">Dashboard</Link></li>
         </ul>
       </nav>
 
