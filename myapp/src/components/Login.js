9367779038d7d2858cae3471585be9ee96f03e74
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-28 07:49:06 +0000

    Validates login credentials

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login(){
@@ -9,5 +9,14 @@ export default function Login(){
         setUser((prevUser)=>({
             ...prevUser,[name]:value,
         }))
+    }
+
+    const authenticate=()=>{
+        if(useReducer.username === "Admin" && user.pwd === "abc@123"){
+            setUser({username: "", pwd: "",error:""})
+            navigate('/dash');
+        }else{
+            setUser({'error':"Please check your username and password"})
+        }
     }
 }
